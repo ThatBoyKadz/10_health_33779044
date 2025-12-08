7e623726891d41bc6534c8125cd9f0df94f028cf
ThatBoyKadz <Kadirdanny@yahoo.co.uk>
2025-12-08 19:09:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -56,9 +56,6 @@ app.use('/users', usersRoutes);
 const workoutsRoutes = require('./routes/workouts');
 app.use('/workouts', workoutsRoutes);
 
-const apiRoutes = require('./routes/api');
-app.use('/api', apiRoutes);
-
 const weatherRoutes = require('./routes/weather');
 app.use('/weather', weatherRoutes);
 
